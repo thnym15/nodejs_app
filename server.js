6f754db232bfe4e444871cc48d5efc90db0fa117
Supapong Putthayangkura <bigsupp@gmail.com>
2019-08-05 18:38:15 +0700

    fix res.send

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-  res.write('helloworld')
+  res.send('helloworld')
 })
 
 const port = process.env.port || 8000
